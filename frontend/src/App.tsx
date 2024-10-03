15e410f6f53083ba4ac02cd7ca5235ff55e06461
Dannynsikak <nsikakdanny11@gmail.com>
2024-10-03 12:18:28 +0100

    eCommerceWebbApp first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>TS Amazona</header>
+      <footer>All right reserverd</footer>
+    </div>
+  );
+}
+
+export default App;
